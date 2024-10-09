6372114b65127a487d0f12168bfdcb98e73cdb6c
Alex Markham <alex.markham@causal.dev>
2024-10-09 16:46:12 +0200

    csv for boxplot for fixed graphs runs

--- workflow/scripts/model_select_ncfa.py
+++ workflow/scripts/model_select_ncfa.py
@@ -1,16 +1,22 @@
 from os import path
+import shutil
 
 import pandas as pd
 
 # join results
 cv_losses = []
-for path in snakemake.input:
+for path in snakemake.input.cv_loss:
     cv_losses.append(pd.read_csv(path))
 
 cv_losses_df = pd.concat(cv_losses)
 cv_losses_df.reset_index(drop=True, inplace=True)
-selected_df = cv_losses_df.loc[[cv_losses_df["avg_cv_loss"].idxmin()]]
+
+selected_idx = cv_losses_df["avg_cv_loss"].idxmin()
+selected_df = cv_losses_df.loc[[selected_idx]]
+
+selected_biadj_weights = snakemake.input.pooled_biadj_weights[selected_idx]
 
 # output
 cv_losses_df.to_csv(snakemake.output["cv_losses"], index=False)
 selected_df.to_csv(snakemake.output["selected_hyperparams"], index=False)
+shutil.copyfile(selected_biadj_weights, snakemake.output["selected_biadj_weights"])
---
+++ workflow/scripts/sfd_cross_validated.py
@@ -0,0 +1,30 @@
+from medil.evaluate import sfd
+import numpy as np
+import pandas as pd
+
+
+true_biadj = np.loadtxt(str(snakemake.input.true_biadj), dtype="bool", delimiter=",")
+if len(true_biadj.shape) == 1:
+    true_biadj = np.expand_dims(true_biadj, axis=0)
+
+est_biadj_weights = np.loadtxt(str(snakemake.input.est_biadj_weights), delimiter=",")
+
+# auto-threshold for SFD
+min_sfd = 9999
+for thresh in np.linspace(
+    np.abs(est_biadj_weights).min(), np.abs(est_biadj_weights).max(), 20
+):
+    biadj_zero_pattern = (np.abs(est_biadj_weights) > thresh).astype(int)
+    sfd_value, _ = sfd(biadj_zero_pattern, true_biadj)
+    min_sfd = min([min_sfd, sfd_value])
+
+# output
+sfd_df = pd.DataFrame(
+    {
+        "sfd": [min_sfd],
+        "Graph": [snakemake.wildcards["benchmark"]],
+        "seed": [snakemake.wildcards["seed"]],
+        "num_samps": [snakemake.wildcards["n"]],
+    }
+)
+sfd_df.to_csv(snakemake.output["sfd"], index=False)
