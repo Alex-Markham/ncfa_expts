a3ed6f63603b6c0c538c6f442328f661137cd028
Alex Markham <alex.markham@causal.dev>
2024-10-10 15:28:49 +0200

    debugging heatmaps; formatting; clean up

---
+++ workflow/scripts/eval_grid.py
@@ -0,0 +1,39 @@
+from medil.evaluate import sfd
+import numpy as np
+import pandas as pd
+
+# input
+true_biadj = np.loadtxt(str(snakemake.input.true_biadj), dtype="bool", delimiter=",")
+if len(true_biadj.shape) == 1:
+    true_biadj = np.expand_dims(true_biadj, axis=0)
+
+est_biadj_weights = np.loadtxt(str(snakemake.input.est_biadj_weights), delimiter=",")
+
+cv_loss = pd.read_csv(snakemake.input.cv_loss)["avg_cv_loss"]
+
+losses = pd.read_csv(snakemake.input.losses)
+end_losses = losses.tail(1)
+
+# auto-threshold for SFD
+min_sfd = 9999
+for thresh in np.linspace(
+    np.abs(est_biadj_weights).min(), np.abs(est_biadj_weights).max(), 20
+):
+    biadj_zero_pattern = (np.abs(est_biadj_weights) > thresh).astype(int)
+    sfd_value, _ = sfd(biadj_zero_pattern, true_biadj)
+    min_sfd = min([min_sfd, sfd_value])
+
+# output
+eval_df = pd.DataFrame(
+    {
+        "graph": [snakemake.wildcards["benchmark"]],
+        "num_samps": [snakemake.wildcards["n"]],
+        "mu": [snakemake.wildcards["mu"]],
+        "lambda": [snakemake.wildcards["llambda"]],
+        "cv_loss": [cv_loss],
+        "sfd": [min_sfd],
+    }
+)
+eval_df = pd.concat([eval_df, end_losses])
+
+eval_df.to_csv(snakemake.output, index=False)
--- workflow/scripts/join_csv.py
+++ workflow/scripts/join_csv.py
@@ -1,13 +1,7 @@
-from os import path
-
 import pandas as pd
 
 # join results
-results = []
-for path in snakemake.input:
-    results.append(pd.read_csv(path))
-
-eval_df = pd.concat(results)
+eval_df = pd.concat([pd.read_csv(path) for path in snakemake.input])
 
 # output
 eval_df.to_csv(snakemake.output[0], index=False)
