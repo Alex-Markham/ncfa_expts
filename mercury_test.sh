2c48f8c435692bdecfb74750df039aa05bb2e6af
Alex Markham <alex.markham@causal.dev>
2024-10-30 10:13:58 -0500

    working with slurm on booth mercury cluster

---
+++ mercury_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --account=pi-naragam
+#SBATCH --partition=standard
+#SBATCH --cpus-per-task=1
+#SBATCH --time=0-04:00:00
+
+#SBATCH --job-name=snakemake_test
+
+module load python/booth/3.12
+cd ~/ncfa_expts/
+source .venv/bin/activate
+
+snakemake --profile ./ all
